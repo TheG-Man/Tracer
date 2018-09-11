7bc96967387598060bb2b1f280ac9c812725d995
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-09-12 00:30:22 +0300

    Add TracedThread class definition

---
+++ Tracer/TracedThread.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Tracer
+{
+    class TracedThread
+    {
+        private readonly List<TracedMethod> tracedMethods;
+        private readonly Stack<TracedMethod> stack;
+
+        private void AddTracedMethod(MethodBase method)
+        {
+            if (stack.Count == 0)
+            {
+                tracedMethods.Add(new TracedMethod(method));
+            }
+            else
+            {
+                stack.Peek().AddNestedMethod(method);
+            }
+
+            stack.Push(new TracedMethod(method));        
+        }
+
+        internal TracedThread()
+        {
+            tracedMethods = new List<TracedMethod>();
+        }
+
+        internal void StopTrace()
+        {
+            if (stack.Count == 0)
+            {
+                throw new InvalidOperationException("There is not method which is executing at the moment.");
+            }
+
+            stack.Peek().StopTrace();
+            stack.Pop();
+        }
+
+        public static TracedThread operator+(TracedThread lhs, MethodBase rhs)
+        {
+            lhs.AddTracedMethod(rhs);
+            return lhs;
+        }
+
+    }
+}
