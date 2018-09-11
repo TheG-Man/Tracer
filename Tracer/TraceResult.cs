f702d5d7f239db5c2bb73cee265ce9efe8751d9e
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-09-12 00:31:06 +0300

    Add TraceResult class definition

---
+++ Tracer/TraceResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Threading;
+using System.Linq;
+using System.Text;
+
+
+namespace Tracer
+{
+    public sealed class TraceResult
+    {
+        private readonly ConcurrentDictionary<int, TracedThread> tracedThreads;
+
+        internal TraceResult()
+        {
+            tracedThreads = new ConcurrentDictionary<int, TracedThread>();
+        }
+
+        internal void AddTracedMethod(MethodBase method)
+        {
+            TracedThread tracedThread = tracedThreads.GetOrAdd(Thread.CurrentThread.ManagedThreadId, new TracedThread());
+            tracedThread += method;
+        }
+
+        internal void StopTrace()
+        {
+            TracedThread tracedThread;
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            if (!tracedThreads.TryGetValue(threadId, out tracedThread))
+            {
+                throw new ArgumentException("There is not a thread " + threadId);
+            }
+
+            tracedThread.StopTrace();
+        }
+
+        internal IEnumerable<KeyValuePair<int, TracedThread>> TracedThreads => tracedThreads;
+    }
+}
