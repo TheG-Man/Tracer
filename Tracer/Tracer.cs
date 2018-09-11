62eb5eeefe4e0da4ad1cb024bf2eefbf411d529c
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-09-12 00:14:36 +0300

    Add Tracer class definition

--- Tracer/Tracer.cs
+++ Tracer/Tracer.cs
@@ -3,10 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace Tracer
 {
-    public class Class1
+    public sealed class Tracer : ITracer
     {
+        private readonly TraceResult traceResult;
+
+        private static volatile Tracer instance = null;
+        private static readonly object syncRoot = new object();
+
+        private Tracer()
+        {
+            traceResult = new TraceResult();
+        }
+
+        public void StartTrace()
+        {
+            var stackTrace = new StackTrace(1);
+            StackFrame stackFrame = stackTrace.GetFrame(0);
+            MethodBase method = stackFrame.GetMethod();
+
+            traceResult.AddTracedMethod(method);
+        }
+
+        public void StopTrace()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return traceResult;
+        }
+
+        public static Tracer GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Tracer();
+                    }
+                }
+            }
+
+            return instance;
+        }
     }
 }
