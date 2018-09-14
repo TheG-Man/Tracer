9f732cdb33b46429e7e31b2b7b908f96c30be1a0
Vadim Seledets <TheG-Man@users.noreply.github.com>
2018-09-14 11:24:50 +0300

    Add classes for result writing

---
+++ TracerTester/ConsoleTraceResultWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public sealed class ConsoleTraceResultWriter : ITraceResultWriter
+    {
+        public void Write(MemoryStream stream)
+        {
+            stream.Position = 0;
+            Console.Write(System.Text.Encoding.Default.GetString(stream.ToArray()));
+        }
+    }
+}
---
+++ TracerTester/FileTraceResultWriter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public sealed class FileTraceResultWriter : ITraceResultWriter
+    {
+        private readonly String fileName;
+
+        public FileTraceResultWriter(String fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void Write(MemoryStream stream)
+        {
+            var fileStream = new FileStream(fileName, FileMode.Create);
+
+            try
+            {
+                stream.Position = 0;
+                stream.CopyTo(fileStream);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                fileStream.Close();
+            }
+        }
+    }
+}
